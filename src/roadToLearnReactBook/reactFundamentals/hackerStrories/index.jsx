ed7c61b788fcc58789d6c6712e9c2e0f9acf769d
feisal-Codes <feisalmib@gmail.com>
2023-10-06 10:27:46 +0300

    server side search

--- src/roadToLearnReactBook/reactFundamentals/hackerStrories/index.jsx
+++ src/roadToLearnReactBook/reactFundamentals/hackerStrories/index.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useReducer, useState } from "react";
 import Search from "./components/search";
 import List from "./components/list";
+import useLocalStorage from "./hooks/useLocalStorage";
 let url = "https://hn.algolia.com/api/v1/search?query=story&page=1";
 const SET_STORIES = "SET_STORIES";
 const SET_INIT_LOADING = "SET_INIT_LOADING";
 const SET_FETCH_FAIL = "SET_FETCH_FAIL";
 const SET_REMOVE_STORY = "SET_REMOVE_STORY";
 const SEARCH_STORIES = "SEARCH_STORIES";
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const HackerStories = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useLocalStorage("searchTerm", "");
+
   const [timeOutId, setTimeOutId] = useState(null);
 
   const reducer = (state, action) => {
@@ -28,14 +31,14 @@ const HackerStories = () => {
           isError: false,
         };
       }
-      case SEARCH_STORIES: {
-        return {
-          ...state,
-          searchedData: state.data.filter((item) =>
-            item.title.toLowerCase().includes(action.payload)
-          ),
-        };
-      }
+      // case SEARCH_STORIES: {
+      //   return {
+      //     ...state,
+      //     searchedData: state.data.filter((item) =>
+      //       item.title.toLowerCase().includes(action.payload)
+      //     ),
+      //   };
+      // }
       case SET_REMOVE_STORY: {
         return {
           ...state,
@@ -57,14 +60,14 @@ const HackerStories = () => {
 
   const [stories, storiesDispatcher] = useReducer(reducer, {
     data: [],
-    searchedData: [],
+    // searchedData: [],
     isLoading: false,
     isError: false,
   });
 
   const fetchData = async () => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(`${API_ENDPOINT}${searchTerm}`);
       const result = await response.json();
       storiesDispatcher({
         type: SET_STORIES,
@@ -83,31 +86,41 @@ const HackerStories = () => {
     setSearchTerm(value);
   };
 
+  // useEffect(() => {
+  //   if (searchTerm === "") return;
+  //   storiesDispatcher({
+  //     type: SET_INIT_LOADING,
+  //     payload: true,
+  //   });
+  //   fetchData();
+  // }, [searchTerm]);
+
   useEffect(() => {
+    if (!searchTerm) {
+      return;
+    }
     storiesDispatcher({
       type: SET_INIT_LOADING,
       payload: true,
     });
-    fetchData();
-  }, []);
-
-  useEffect(() => {
     clearTimeout(timeOutId);
+
     let newTimeOutId = setTimeout(() => {
-      storiesDispatcher({
-        type: SEARCH_STORIES,
-        payload: searchTerm,
-      });
-    }, 300);
+      // storiesDispatcher({
+      //   type: SEARCH_STORIES,
+      //   payload: searchTerm,
+      // });
+      fetchData();
+    }, 200);
     if (newTimeOutId) {
       setTimeOutId(newTimeOutId);
     }
 
-    if (searchTerm === "") {
-      storiesDispatcher({
-        type: "default",
-      });
-    }
+    // if (searchTerm === "") {
+    //   storiesDispatcher({
+    //     type: "default",
+    //   });
+    // }
 
     return () => clearTimeout(newTimeOutId);
   }, [searchTerm]);
@@ -126,11 +139,7 @@ const HackerStories = () => {
       ) : (
         <div>
           <List
-            list={
-              searchTerm && !stories.isError
-                ? stories.searchedData
-                : stories.data
-            }
+            list={stories.data}
             storiesDispatcher={storiesDispatcher}
             actionType={SET_REMOVE_STORY}
           />
