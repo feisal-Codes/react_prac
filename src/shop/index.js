20c4806db60889916e89fc897ef7095dfc474ee6
Feisal Mohamed <feisalmib@gmail.com>
2024-02-03 02:02:55 +0300

    Update index.js

--- src/shop/index.js
+++ src/shop/index.js
@@ -36,16 +36,34 @@ const Shop = ({ handleClick }) => {
     <>
       <Header />
       <div style={{ display: "flex", padding: "40px" }}>
-        <div style={{ flex: 1, marginLeft: "220px" }}> {/* Add margin to create space for fixed filters */}
+        <div style={{ flex: 1, marginLeft: "220px" }}>
+          {" "}
+          {/* Add margin to create space for fixed filters */}
           {filteredItems.length ? (
             <Products handleAddToCart={handleClick} data={filteredItems} />
           ) : (
             <Products handleAddToCart={handleClick} data={data} />
           )}
         </div>
-        <div style={{ width: "200px", marginLeft: "20px", position: "fixed", overflowY: "scroll", height: "100vh" }}>
-          <div style={{ padding: "10px", border: "1px solid #e2e8f0", borderRadius: "8px" }}>
-            <h4 style={{ marginBottom: "10px", textAlign: "center" }}>Filter On Pricing</h4>
+        <div
+          style={{
+            width: "200px",
+            marginLeft: "20px",
+            position: "fixed",
+            overflowY: "scroll",
+            height: "100vh",
+          }}
+        >
+          <div
+            style={{
+              padding: "10px",
+              border: "1px solid #e2e8f0",
+              borderRadius: "8px",
+            }}
+          >
+            <h4 style={{ marginBottom: "10px", textAlign: "center" }}>
+              Filter On Pricing
+            </h4>
 
             <div style={{ marginBottom: "8px" }}>
               <div className="flex">
@@ -56,7 +74,9 @@ const Shop = ({ handleClick }) => {
                   value={filters.price}
                   onChange={handleChange}
                 />
-                <label htmlFor="low" style={{ marginLeft: "5px" }}>Low to High</label>
+                <label htmlFor="low" style={{ marginLeft: "5px" }}>
+                  Low to High
+                </label>
               </div>
               <div className="flex">
                 <input
@@ -66,7 +86,9 @@ const Shop = ({ handleClick }) => {
                   value={filters.price}
                   onChange={handleChange}
                 />
-                <label htmlFor="high" style={{ marginLeft: "5px" }}>High to Low</label>
+                <label htmlFor="high" style={{ marginLeft: "5px" }}>
+                  High to Low
+                </label>
               </div>
               <div className="flex">
                 <input
@@ -76,7 +98,9 @@ const Shop = ({ handleClick }) => {
                   value={filters.price}
                   onChange={handleChange}
                 />
-                <label htmlFor="default" style={{ marginLeft: "5px" }}>Default</label>
+                <label htmlFor="default" style={{ marginLeft: "5px" }}>
+                  Default
+                </label>
               </div>
             </div>
           </div>
